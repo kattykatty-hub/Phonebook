22a8a2101e9a3970c08566263513b2a6c0cbfcf7
kattykatty-hub <katrinkatkatrin@yahoo.com>
2024-10-11 16:27:09 +0200

    whitelist: ["items"] added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { contactsReducer } from './contactsSlice';
-import { filtersReducer } from './filtersSlice';
-import { combineReducers } from 'redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { contactsReducer } from "./contactsSlice";
+import { filtersReducer } from "./filtersSlice";
+import { combineReducers } from "redux";
 
 const persistConfig = {
-  key: 'root', 
+  key: "root",
   storage,
-  whitelist: ['contacts'], 
+  whitelist: ["items"],
 };
 
 const rootReducer = combineReducers({
-  contacts: persistReducer(persistConfig, contactsReducer), 
+  contacts: persistReducer(persistConfig, contactsReducer),
   filters: filtersReducer,
 });
 
@@ -20,7 +20,7 @@ export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, 
+      serializableCheck: false,
     }),
 });
 
